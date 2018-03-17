01ca1dfa815a76696916f22cbd5da1a3336e5e53
vcshahriyer <shahriyer99@gmail.com>
2018-03-17 13:04:46 +0600

    Pyramid 3

---
+++ Pyramids/Pyramid_3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int row=3;
+
+
+    for (int i=row;i>=1;i--){
+
+        for (int point=i;point>1;point--){
+            cout<<". ";
+        }
+        for (int star=i;star<=row;star++){
+            cout<<"* ";
+        }
+
+        for (int star=i;star<row;star++){
+            cout<<"* ";
+        }
+        for (int point=i;point>1;point--){
+            cout<<". ";
+        }
+        cout<<"\n";
+
+    }
+    for (int i=2;i<=row;i++){
+
+        for (int point=i;point>1;point--){
+            cout<<". ";
+        }
+        for (int star=i;star<=row;star++){
+            cout<<"* ";
+        }
+
+        for (int star=i;star<row;star++){
+            cout<<"* ";
+        }
+        for (int point=i;point>1;point--){
+            cout<<". ";
+        }
+        cout<<"\n";
+
+    }
+
+
+}
