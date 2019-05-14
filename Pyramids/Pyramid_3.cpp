d8cfcd975ef66603b810531e8b6007be5914352f
Raghib Shahriyer <vcshahriyer@gmail.com>
2019-05-14 14:37:07 +0600

    fixed untracked file

--- Pyramids/Pyramid_3.cpp
+++ Pyramids/Pyramid_3.cpp
@@ -1,47 +1,53 @@
+// for 5 rows.
+// . . . . * . . . .
+// . . . * * * . . .
+// . . * * * * * . .
+// . * * * * * * * .
+// * * * * * * * * *
+// . * * * * * * * .
+// . . * * * * * . .
+// . . . * * * . . .
+// . . . . * . . . .
+
 #include <iostream>
 using namespace std;
 
-int main(){
+int main() {
 
-    int row=3;
+  int row = 5;
 
+  for (int i = row; i >= 1; i--) {
 
-    for (int i=row;i>=1;i--){
-
-        for (int point=i;point>1;point--){
-            cout<<". ";
-        }
-        for (int star=i;star<=row;star++){
-            cout<<"* ";
-        }
-
-        for (int star=i;star<row;star++){
-            cout<<"* ";
-        }
-        for (int point=i;point>1;point--){
-            cout<<". ";
-        }
-        cout<<"\n";
-
+    for (int point = i; point > 1; point--) {
+      cout << ". ";
     }
-    for (int i=2;i<=row;i++){
-
-        for (int point=i;point>1;point--){
-            cout<<". ";
-        }
-        for (int star=i;star<=row;star++){
-            cout<<"* ";
-        }
-
-        for (int star=i;star<row;star++){
-            cout<<"* ";
-        }
-        for (int point=i;point>1;point--){
-            cout<<". ";
-        }
-        cout<<"\n";
-
+    for (int star = i; star <= row; star++) {
+      cout << "* ";
     }
 
+    for (int star = i; star < row; star++) {
+      cout << "* ";
+    }
+    for (int point = i; point > 1; point--) {
+      cout << ". ";
+    }
+    cout << "\n";
+  }
+  for (int i = 2; i <= row; i++) {
 
+    for (int point = i; point > 1; point--) {
+      cout << ". ";
+    }
+    for (int star = i; star <= row; star++) {
+      cout << "* ";
+    }
+
+    for (int star = i; star < row; star++) {
+      cout << "* ";
+    }
+    for (int point = i; point > 1; point--) {
+      cout << ". ";
+    }
+    cout << "\n";
+  }
 }
